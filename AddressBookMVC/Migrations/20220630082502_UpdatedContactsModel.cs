9ffcf53e8ae40a59a512192cb61e6a2d2e77c391
Jonathan Baggott <jonathan.baggott82@gmail.com>
2022-06-30 09:42:36 +0100

    Add project files.

---
+++ AddressBookMVC/Migrations/20220630082502_UpdatedContactsModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressBookMVC.Migrations
+{
+    public partial class UpdatedContactsModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "Contacts",
+                newName: "PostCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "County",
+                table: "Contacts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "County",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "PostCode",
+                table: "Contacts",
+                newName: "State");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Zip",
+                table: "Contacts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ AddressBookMVC/Services/DefaultImageService.cs
@@ -0,0 +1,26 @@
+﻿using AddressBookMVC.Services.Interfaces;
+
+namespace AddressBookMVC.Services
+{
+    public class DefaultImageService : IImageService
+    {
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            if (fileData is null)
+            {
+                return string.Empty;
+            }
+
+            string imageBase64Data = Convert.ToBase64String(fileData);
+            return $"data:{extension};base64,{imageBase64Data}";
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            byte[] byteFile = memoryStream.ToArray();
+            return byteFile;
+        }
+    }
+}
